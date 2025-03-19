55f832ea8dc5a27480fb26666f0ab53e0fbc8738
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2025-03-19 16:40:55 +0100

    Switching page system

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log("test");
+import { loadHomePage } from "./home-page-load.js";
+import { loadMenuPage } from "./menu-page-load.js";
+import { loadAboutPage } from "./about-page-load.js";
+
+loadHomePage();
+
+const container = document.getElementById("content");
+
+const homeBtn = document.getElementById("home");
+
+homeBtn.addEventListener("click", () => {
+    container.innerHTML = "";
+    loadHomePage();
+});
+
+const menuBtn = document.getElementById("menu");
+
+menuBtn.addEventListener("click", () => {
+    container.innerHTML = "";
+    loadMenuPage();
+});
+
+const aboutMenu = document.getElementById("about");
+
+aboutMenu.addEventListener("click", () => {
+    container.innerHTML = "";
+    loadAboutPage();
+});
