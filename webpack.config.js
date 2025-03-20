8d601d5a4e371136e4ea00d21ed20997a99a759f
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2025-03-20 13:16:40 +0100

    Adding css loaders in config file

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.css/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
                 test: /\.png$/i,
                 type: "asset/resource",
             },
